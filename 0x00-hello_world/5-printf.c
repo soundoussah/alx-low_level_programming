c4d9ab03c9331e6edcf1af739f2fac556e60d8fb
soundoussah <elazzioui.soundouss@gmail.com>
2023-03-16 07:32:31 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - this is hello printf
+ *
+ * Return: 0 is ok
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+	return (0); }
